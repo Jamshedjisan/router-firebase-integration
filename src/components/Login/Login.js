2fe4a2989ffda409aea7b735616c29e004ef2bc7
Jamshedjisan <jamshedjisan76@gmail.com>
2022-04-11 23:38:13 +0600

    added authstate changed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Login = () => {
+  const { signInWithGoogle } = useFirebase();
   return (
     <div>
       <h2>This is Login page</h2>
+      <br />
+      <button onClick={signInWithGoogle}>Google Sign in</button>
+
+      <form>
+        <input type="email" placeholder="Your email" />
+        <br />
+        <input type="password" name="" id="" placeholder="Password" />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 };
